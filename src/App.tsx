72739c95ff7f56f91450a1a6d2c6c076e1846602
KaiWelu <kaiweluda@gmail.com>
2024-03-19 10:32:40 +0100

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>This will be the homepage for das Segelzentrum</div>;
+  return (
+    <div className="lg:text-3xl lg:text-cyan-700 font-bold underline">
+      This will be the homepage for das Segelzentrum
+    </div>
+  );
 }
 
 export default App;
