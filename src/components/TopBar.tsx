63f972cefd5afba7be06102a1b80cb049b1d8a05
KaiWelu <kaiweluda@gmail.com>
2024-03-20 16:50:20 +0100

    feat: adds design for the top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,25 +2,26 @@ import { NavLink } from "react-router-dom";
 
 const TopBar = () => {
   return (
-    <>
-      <div className="flex flex-row justify-center bg-white">
-        <div className="w-full md:max-w-7xl py-4 flex flex-row">
+    <div>
+      <div className="flex flex-row justify-center bg-white ">
+        <div className="w-full md:max-w-7xl py-4 flex flex-row gap-4  border-primary-2">
           <img
-            src="/public/assets/logo/logo_full_color.png"
+            src="/public/assets/logo/logo_transparent_bg.png"
             alt="logo"
             width={150}
             height={150}
+            className="p-1"
           />
-          <div className="flex flex-col gap-1 justify-center">
-            <p className="text-5xl text-primary-1">
-              CVJM Freizeit- und Segelzentrum Ratzeburg
+          <div className="flex flex-col gap-2 ">
+            <p className="text-5xl text-primary-1 font-light">CVJM Ratzeburg</p>
+            <p className="text-4xl  text-secondary-1  font-light">
+              Freizeit- und Segelzentrum
             </p>
-            {/* <p className="text-4xl">Ratzeburg</p> */}
           </div>
         </div>
       </div>
-      <nav className="bg-primary-2 flex flex-row justify-center">
-        <div className="w-full md:max-w-7xl flex flex-row gap-6 justify-start">
+      <nav className="bg-white flex flex-row justify-center ">
+        <div className="w-full md:max-w-7xl flex flex-row gap-6 justify-start border-b-2 border-t-2 border-primary-2">
           <NavLink to="haus" className="navlink">
             Haus
           </NavLink>
@@ -41,7 +42,7 @@ const TopBar = () => {
           </NavLink>
         </div>
       </nav>
-    </>
+    </div>
   );
 };
 
