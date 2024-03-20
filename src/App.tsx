db54fb6f5b31f04f9261874ca406638f0a5a0187
KaiWelu <kaiweluda@gmail.com>
2024-03-20 10:51:03 +0100

    chore: setup for paths

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,24 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import RootLayout from "./_root/pages/RootLayout";
+import Home from "./_root/pages/Home";
+import Haus from "./_root/pages/Haus";
+import Wassersport from "./_root/pages/Wassersport";
+import Umgebung from "./_root/pages/Umgebung";
+import Buchung from "./_root/pages/Buchung";
+import Verein from "./_root/pages/Verein";
+import Partner from "./_root/pages/Partner";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      {" "}
+      <Route index element={<Home />} />
+      <Route path="haus" element={<Haus />} />
+      <Route path="wassersport" element={<Wassersport />} />
+      <Route path="umgebung" element={<Umgebung />} />
+      <Route path="buchung" element={<Buchung />} />
+      <Route path="verein" element={<Verein />} />
+      <Route path="partner" element={<Partner />} />
     </Route>
   )
 );
