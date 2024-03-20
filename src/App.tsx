e6f8d57db94cb21b5a29b4470af09d6fe0f4cfdb
KaiWelu <kaiweluda@gmail.com>
2024-03-20 10:21:02 +0100

    installs react router

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,19 @@ function App() {
       <div className="lg:text-3xl font-bold underline text-secondary-1">
         This will be the homepage for das Segelzentrum
       </div>
-      <p>
+      <p className="text-primary-2">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
         aliquam fugit, placeat obcaecati, numquam culpa temporibus quod
         architecto velit vel voluptatem doloremque accusantium eveniet
         laboriosam fuga perspiciatis beatae tenetur tempore!
       </p>
-      <p>
+      <p className="text-primary-1">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+        aliquam fugit, placeat obcaecati, numquam culpa temporibus quod
+        architecto velit vel voluptatem doloremque accusantium eveniet
+        laboriosam fuga perspiciatis beatae tenetur tempore!
+      </p>
+      <p className="text-secondary-1">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
         aliquam fugit, placeat obcaecati, numquam culpa temporibus quod
         architecto velit vel voluptatem doloremque accusantium eveniet
