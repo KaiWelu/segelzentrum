c3d35ca8f34d1c759894ddbe7f2083d78953380d
KaiWelu <kaiweluda@gmail.com>
2024-03-25 22:36:16 +0100

    feat: adds home gallery and fixes z issue

--- src/_root/pages/Haus.tsx
+++ src/_root/pages/Haus.tsx
@@ -1,32 +1,110 @@
 const Haus = () => {
   return (
     <div>
-      <div className="lg:text-3xl font-bold underline text-primary-1">
-        This will be the homepage for das Segelzentrum
-      </div>
-      <div className="lg:text-3xl font-bold underline text-primary-2">
-        This will be the homepage for das Segelzentrum
-      </div>
-      <div className="lg:text-3xl font-bold underline text-secondary-1">
-        This will be the homepage for das Segelzentrum
-      </div>
-      <p className="text-primary-2">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-        aliquam fugit, placeat obcaecati, numquam culpa temporibus quod
-        architecto velit vel voluptatem doloremque accusantium eveniet
-        laboriosam fuga perspiciatis beatae tenetur tempore!
-      </p>
-      <p className="text-primary-1">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-        aliquam fugit, placeat obcaecati, numquam culpa temporibus quod
-        architecto velit vel voluptatem doloremque accusantium eveniet
-        laboriosam fuga perspiciatis beatae tenetur tempore!
-      </p>
-      <p className="text-secondary-1">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-        aliquam fugit, placeat obcaecati, numquam culpa temporibus quod
-        architecto velit vel voluptatem doloremque accusantium eveniet
-        laboriosam fuga perspiciatis beatae tenetur tempore!
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
+      </p>{" "}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
+        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
+        rem odit ea soluta quae libero, dolor et unde inventore?
       </p>
     </div>
   );
--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,131 +1,45 @@
-// Import Swiper React components
+// import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
+// import Swiper styles
 import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Home = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>{" "}
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quas
-        placeat sint explicabo fugiat aliquid nobis iusto sunt laudantium non
-        rem odit ea soluta quae libero, dolor et unde inventore?
-      </p>
+    <div className="p-1 mt-6">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src="/public/assets/images/hero-carousel/cvjm_impression_1.jpg"
+            alt="sailing"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="/public/assets/images/hero-carousel/cvjm_impression_2.jpg"
+            alt="sailing"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
-    // <div className="m-4 flex justify-items-start">
-    //   <Swiper className="mySwiper w-xl bg-primary-2 h-96 ">
-    //     <SwiperSlide>Slide 1</SwiperSlide>
-    //     <SwiperSlide>Slide 2</SwiperSlide>
-    //     <SwiperSlide>Slide 3</SwiperSlide>
-    //     <SwiperSlide>Slide 4</SwiperSlide>
-    //     <SwiperSlide>Slide 5</SwiperSlide>
-    //     <SwiperSlide>Slide 6</SwiperSlide>
-    //     <SwiperSlide>Slide 7</SwiperSlide>
-    //     <SwiperSlide>Slide 8</SwiperSlide>
-    //     <SwiperSlide>Slide 9</SwiperSlide>
-    //   </Swiper>
-    // </div>
   );
 };
 
