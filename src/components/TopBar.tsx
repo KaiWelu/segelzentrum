04a7119b9e5596a2c0f8022980870dc94f4f8cfe
KaiWelu <kaiweluda@gmail.com>
2024-03-26 16:22:41 +0100

    feat: adds better styling for the submenu

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -9,7 +9,7 @@ const TopBar = () => {
   }
 
   return (
-    <div className="w-full bg-white shadow-lg shadow-slate-300 ">
+    <div className="w-full bg-white shadow-sm shadow-slate-300 relative">
       <nav className="navbar mx-auto max-w-screen-xl py-5">
         {/* desktop navbar elements */}
         <div className="navbar-start">
@@ -42,8 +42,15 @@ const TopBar = () => {
           <p>here be dragons</p>
         </div>
       </nav>
+      {!isHausMenuShown && (
+        <nav className="bg-primary-2 text-white h-12 absolute -z-10 top-16 w-full">
+          Hallo
+        </nav>
+      )}
       {isHausMenuShown && (
-        <nav className="bg-primary-2 text-white h-12">Hallo</nav>
+        <nav className="bg-primary-2 text-white h-16 absolute -z-10 top-30 w-full shadow-sm shadow-slate-300 ">
+          Hallo
+        </nav>
       )}
     </div>
   );
