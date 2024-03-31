aba7ab2b5befe89632599693e261705fbc0dee3a
KaiWelu <kaiweluda@gmail.com>
2024-03-31 16:00:25 +0200

    feat: adds better layout for anfahrt page

--- src/_root/pages/haus/Anfahrt.tsx
+++ src/_root/pages/haus/Anfahrt.tsx
@@ -1,6 +1,6 @@
 const Anfahrt = () => {
   return (
-    <div className="content-wrapper">
+    <div className="content-wrapper min-h-lvh">
       <section className="content-section">
         <h1>Anfahrt</h1>
         <p>
@@ -14,18 +14,18 @@ const Anfahrt = () => {
           Bahn AG (bahn.de) oder des Hamburger Verkehrsverbundes (hvv.de)
           abrufen.
         </p>
-        <div className="md:p-2 mt-4">
-          <iframe
-            className="w-full h-80"
-            id="gmap_canvas"
-            src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=CVJM-Freizeit%20und%20Segelzentrum%20Ratzeburg%20gemeinn%C3%BCtzige%20GmbH%20Berlin+(CVJM-Freizeit%20und%20Segelzentrum%20Ratzeburg%20gemeinn%C3%BCtzige%20GmbH)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-          ></iframe>
-          <script
-            type="text/javascript"
-            src="https://embedmaps.com/google-maps-authorization/script.js?id=db4b7973a24d9318dbd965e5df06a7f489f39218"
-          ></script>
-        </div>
       </section>
+      <div className="md:p-2 mt-4 w-full md:h-dvh">
+        <iframe
+          className="w-full md:h-4/6 h-80"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?width=520&amp;height=800&amp;hl=en&amp;q=CVJM-Freizeit%20und%20Segelzentrum%20Ratzeburg%20gemeinn%C3%BCtzige%20GmbH%20Berlin+(CVJM-Freizeit%20und%20Segelzentrum%20Ratzeburg%20gemeinn%C3%BCtzige%20GmbH)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+        <script
+          type="text/javascript"
+          src="https://embedmaps.com/google-maps-authorization/script.js?id=db4b7973a24d9318dbd965e5df06a7f489f39218"
+        ></script>
+      </div>
     </div>
   );
 };
