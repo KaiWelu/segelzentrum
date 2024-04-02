d4d1e1bb7e20355038e9ce4ddb5a8fbd0e07b722
KaiWelu <kaiweluda@gmail.com>
2024-04-02 14:53:11 +0200

    feat: adds verein uber uns page

--- src/_root/pages/verein/VereinUberUns.tsx
+++ src/_root/pages/verein/VereinUberUns.tsx
@@ -1,5 +1,78 @@
 const VereinUberUns = () => {
-  return <div>VereinUberUns</div>;
+  return (
+    <div className="content-wrapper">
+      <section className="content-section">
+        <h1>Segeln im CVJM e.V.</h1>
+        <p>
+          Im Oktober 2001 gründete sich der Förderverein Segeln im CVJM e.V. auf
+          der Vollversammlung der ehrenamtlichen Segellehrer des CVJM Freizeit–
+          und Segelzentrums Ratzeburg, um das CVJM Freizeit- und Segelzentrum
+          Ratzeburg bei der Unterhaltung und Entwicklung des Bootsparks zu
+          unterstützen.
+        </p>
+        <p>
+          Die Motivation des Vereins besteht in der Verbundenheit zum Segelsport
+          im CVJM, insbesondere im Freizeit- und Segelzentrum in Ratzeburg. Der
+          Förderverein gründete sich aus der ehrenamtlichen Arbeit der
+          Segellehrer. Dieses ehrenamtliche Engagement soll gestärkt und
+          gefestigt werden. Dazu gehört vor allem ein intakter und sicherer
+          Bootspark.
+        </p>
+        <p>
+          In jeder Saison müssen die Boote ca. 2000 Segelschüler bewältigen. In
+          den vergangenen 30 Jahren hat der gesamte Bootspark ca. 60.000
+          Menschen über den See getragen und dieser Einsatz hat Spuren
+          hinterlassen. Der Förderverein hat es sich zur Aufgabe gemacht, in den
+          nächsten Jahren mindestens ein neues Boot pro Saison anzuschaffen
+          sowie die bestehende Flotte mit neuen Segeln auszurüsten.
+        </p>
+        <p>
+          Die sportpädagogische Arbeit auf dem Ratzeburger See konnte und kann
+          nur durch externe Hilfe weiter bestehen.
+        </p>
+        <p>
+          Des Weiteren werden neue Mitglieder, Unterstützer und ehrenamtliche
+          Segellehrer gesucht. Jeder, der die Arbeit im Wassersportbereich
+          unterstützen und fördern will, ist herzlich willkommen!
+        </p>
+        <h2 className="text-xl font-semibold mt-4">Wer kann was tun?</h2>
+        <div className="prose prose-md">
+          <ul>
+            <li className="list-disc marker:text-primary-2">
+              Sie haben schöne Tage oder Wochen auf dem Wasser genossen?
+            </li>
+            <li className="list-disc marker:text-primary-2">
+              Das Segelzentrum ist für Sie ein Stück Heimat geworden?
+            </li>
+            <li className="list-disc marker:text-primary-2">
+              Sie sind beeindruckt von dem einzigartigen Engagement der ca.
+              siebzig ehrenamtlichen Segellehrer und möchten deren Arbeit
+              unterstützen?
+            </li>
+            <li className="list-disc marker:text-primary-2">
+              Sie möchten aktiv am Erhalt des Bootsparks und der Förderung des
+              Wassersports mitwirken?
+            </li>
+          </ul>
+        </div>
+        <p>
+          Dann denken Sie doch über eine Spende an den Förderverein nach.
+          Vielleicht möchten Sie auch Mitglied im Verein werden und dadurch den
+          Segelsport mit Kindern, Jugendlichen und Benachteiligten dauerhaft
+          unterstützen?
+        </p>
+        <p>
+          Wir freuen uns auch, wenn Sie mit uns aktiv werden und im eigenen
+          Verein, Kollegium oder Unternehmen für den Förderverein Segeln im CVJM
+          e.V. werben und sammeln möchten.
+        </p>
+        <p>
+          Der Förderverein und unzählige begeisterte Menschen, die im CVJM die
+          Freude am Wassersport kennen lernen, bedanken sich bei Ihnen!
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default VereinUberUns;
